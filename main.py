56660c2018f97e4879925a867dd2701164797a3b
stem league <64894084+stemleague@users.noreply.github.com>
2021-08-18 02:00:06 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,53 @@
+# Welcome to the Movie Collection! At the Movie Collection, we have stored titles and years of movies for you to explore!
+
+# Titles and Years of Movies
+movies = ["Back to the Future", "Train to Busan", "The Mummy", "Shang-Chi and the Legend of the Ten Rings", "Back to the Future", "Train to Busan", "Harry Potter and the Goblet of Fire"]
+years = [1985, 2016, 1999, 2021, 2005]
+
+# Print the list (movies) with the message "Original Movie Collection: "
+
+
+# Print the list (years) with the message "Original Years: "
+
+
+# Print the number of movies in our collection by using the len(list_name) function on the list (movies) with the message "Number of movies: "
+
+
+# Print the number of occurences of the movies "Back to the Future" and "Train to Busan" in (movies) by using the count(value) function
+
+
+# Ah! We have 2 duplicate movies in our collection! Call the remove(value) function on the list (movies) to eliminate "Back to the Future" and "Train to Busan" once each from our collection.
+
+
+# The movie collection is out of order! We want to sort the movies in order by increasing year. 
+
+# First, call the sort() function on the list (years) to sort them in increasing order. Print out the result to make sure the years are sorted!
+
+
+# Then, call the insert(index, value) function on the list (movies) to change the movie titles around to match the order of the list (years). Print along the way to test the output!
+
+
+# Ask the user to enter their favorite movie (hint: input function) and store inside variable called "fave_movie"
+
+
+# Append variable "fave_movie" to the list (movies) 
+
+
+# Ask the user to enter the year their favorite movie was released (hint: input function) and store inside variable called "movie_year"
+
+
+# Append variable "movie_year" to the list (years)
+
+
+# Call the insert(index, value) function if the new movie and new year are out of order in the lists 
+
+
+
+# A customer wants to purchase movies from the years 2000-2020. Use indexing to print the movies from this time period. Can you do this using negative index values?
+
+
+# Here's a memorable quote from one of the movies in our collection. Reverse the string to find out what it is!
+quote = "naeroLeD A fO tuO...enihcaM emiT A tliuB uoY eM gnilleT uoY erA .coD ,etuniM A tiaW"
+
+
+# Well thanks for sorting out our movie collection. See ya next time!
