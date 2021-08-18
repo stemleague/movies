f3f6261d6c428946dfcb29681be1b5b196244d8c
stem league <64894084+stemleague@users.noreply.github.com>
2021-08-18 02:22:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,29 @@
 movies = ["Back to the Future", "Train to Busan", "The Mummy", "Shang-Chi and the Legend of the Ten Rings", "Back to the Future", "Train to Busan", "Harry Potter and the Goblet of Fire"]
 years = [1985, 2016, 1999, 2021, 2005]
 
-# Print the list (movies) with the message "Original Movie Collection: "
+# Print the list (movies) with the message "Original Movie Collection: " (Type cast the list to a string when concatenating!)
 
 
-# Print the list (years) with the message "Original Years: "
+# Print the list (years) with the message "Original Years: " (Type cast the list to a string when concatenating!)
 
 
-# Print the number of movies in our collection by using the len(list_name) function on the list (movies) with the message "Number of movies: "
+# Print the number of movies in our collection by using the len(list_name) function on the list (movies) with the message "Number of movies: " (Type cast the list to a string when concatenating!)
 
 
-# Print the number of occurences of the movies "Back to the Future" and "Train to Busan" in (movies) by using the count(value) function
+# Print the number of occurences of the movies "Back to the Future" and "Train to Busan" in (movies) by calling the count(value) function
+
 
 
 # Ah! We have 2 duplicate movies in our collection! Call the remove(value) function on the list (movies) to eliminate "Back to the Future" and "Train to Busan" once each from our collection.
 
 
+# Print (movies) to make sure output does not include duplicate movies
+
+
 # The movie collection is out of order! We want to sort the movies in order by increasing year. 
 
 # First, call the sort() function on the list (years) to sort them in increasing order. Print out the result to make sure the years are sorted!
+
 
 
 # Then, call the insert(index, value) function on the list (movies) to change the movie titles around to match the order of the list (years). Print along the way to test the output!
@@ -46,8 +51,24 @@ years = [1985, 2016, 1999, 2021, 2005]
 # A customer wants to purchase movies from the years 2000-2020. Use indexing to print the movies from this time period. Can you do this using negative index values?
 
 
+
+# After the customer has purchased these titles, we need to remove them from the collection. Call the remove(value) function on these values.
+
+
+
+# Print the list (movies) with the message "Sorted Movie Collection: " (Type cast the list to a string when concatenating!)
+
+
+# Print the list (years) with the message "Sorted Years: " (Type cast the list to a string when concatenating!)
+
+
+# Print the number of movies in our collection by using the len(list_name) function on the list (movies) with the message "Number of movies: " (Type cast the list to a string when concatenating!)
+
+
+
 # Here's a memorable quote from one of the movies in our collection. Reverse the string to find out what it is!
-quote = "naeroLeD A fO tuO...enihcaM emiT A tliuB uoY eM gnilleT uoY erA .coD ,etuniM A tiaW"
+quote = "?naeroLeD A fO tuO...enihcaM emiT A tliuB uoY eM gnilleT uoY erA .coD ,etuniM A tiaW"
+
 
 
 # Well thanks for sorting out our movie collection. See ya next time!
